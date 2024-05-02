256bd48ee78ae20e0465df2da0237adee1e33a04
Максим Ефимов <efimov_m@t.top-academy.ru>
2024-05-02 10:05:39 +0300

    Добавьте файлы проекта.

---
+++ WeighingProject/WeighingProject.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+    int kettlebellMax{};
+    std::vector<int> set;
+    std::vector<int> left;
+    std::vector<int> right;
+
+    int weight;
+
+    std::cout << "Input weight: ";
+    std::cin >> weight;
+
+    bool isFind{ false };
+    while(true)
+    {
+        
+        
+    }
+}
