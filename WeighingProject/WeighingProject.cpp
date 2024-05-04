accb5ba9ad51006787e724db33c9973a081f339e
Max Efimov <efimov.top.academy@gmail.com>
2024-05-04 10:59:04 +0300

    04-05-24

--- WeighingProject/WeighingProject.cpp
+++ WeighingProject/WeighingProject.cpp
@@ -14,36 +14,5 @@ int main()
     std::cin >> weight;
 
     bool isFind{ false };
-    int amount{};
-    int numbers{};
-    int number{};
-    while(true)
-    {
-        set.clear();
-        for (int i{}; i <= kettlebellMax; i++)
-            set.push_back(pow(3, i));
-
-        numbers = pow(2, set.size());
-        number = 0;
-        while (number < numbers)
-        {
-            isFind = false;
-            amount = 0;
-            int n = number;
-            int i = set.size() - 1;
-            while (n)
-            {
-                amount += (n % 2) * set[i];
-                i--;
-                n /= 2;
-            }
-            if (amount >= weight)
-                isFind = true;
-            if (isFind)
-                break;
-        }
-        if (isFind)
-            break;        
-        kettlebellMax++;
-    }
+   
 }
